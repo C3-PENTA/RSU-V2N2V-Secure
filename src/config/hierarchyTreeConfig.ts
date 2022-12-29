2a8342d5746d871f11ae3d689228a01eed9bf2d2
smlee97 <smlee@pentasecurity.com>
2022-12-29 14:11:01 +0900

    add file : hierarchyTreeConfig

---
+++ src/config/hierarchyTreeConfig.ts
@@ -0,0 +1,6 @@
+import { ENodeStatus, IStatusConfig } from '@/interface/interfaceHierarchyTree';
+
+export const rootCoordinate = { x: 0, y: 0 };
+
+export const defaultNodeWidth = 90;
+export const defaultNodeHeight = 50;

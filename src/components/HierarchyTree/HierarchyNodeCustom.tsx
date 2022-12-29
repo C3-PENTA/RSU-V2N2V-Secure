c77b0907b734ee5e025884daf51933d806cdc8b5
smlee97 <smlee@pentasecurity.com>
2022-12-29 13:57:42 +0900

    update file : HierarchyNodeCustom

--- src/components/HierarchyTree/HierarchyNodeCustom.tsx
+++ src/components/HierarchyTree/HierarchyNodeCustom.tsx
@@ -4,6 +4,7 @@ export const HierarchyNodeCustom = (nodeProps: NodeProps) => {
   return (
     <>
       {nodeProps.data.label}
-      
+      <Handle type="source" position={Position.Bottom} />
+      <Handle type="target" position={Position.Top} />      
   );
 };

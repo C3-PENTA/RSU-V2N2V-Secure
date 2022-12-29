9c05abaf880fc8a4b01ba5f6b1078aaeedf00125
smlee97 <smlee@pentasecurity.com>
2022-12-29 14:14:31 +0900

    update file : listEventConfig

--- src/config/listEventConfig.tsx
+++ src/config/listEventConfig.tsx
@@ -14,3 +14,18 @@ export const filterFormInitVal: IListEventForm = {
   size: paginationConfig.pageSizePool[0],
 };
 
+export const categoryConfig = [
+  {
+    id: 1,
+    label: 'list_event.category.availability_status_tranfer',
+  },
+  {
+    id: 2,
+    label: 'list_event.category.virus',
+  },
+  {
+    id: 3,
+    label: 'list_event.category.communication',
+  },
+];
+

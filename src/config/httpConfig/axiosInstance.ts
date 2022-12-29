f778981859777dd9288282061c7a82abf4886df7
smlee97 <smlee@pentasecurity.com>
2022-12-29 14:09:17 +0900

    add file : axiosInstance

---
+++ src/config/httpConfig/axiosInstance.ts
@@ -0,0 +1,7 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+const httpConfig: AxiosRequestConfig = {
+  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URI,
+  timeout: 10000,
+};

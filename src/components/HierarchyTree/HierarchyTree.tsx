7d148f11c8fbbb50402a524a560633111efe2e57
smlee97 <smlee@pentasecurity.com>
2022-12-29 14:01:20 +0900

    add comments

--- src/components/HierarchyTree/HierarchyTree.tsx
+++ src/components/HierarchyTree/HierarchyTree.tsx
@@ -3,6 +3,14 @@ import { HierarchyTreeCanvas } from './HierarchyTreeCanvas';
 
 import './HierarchyTree.scss';
 
+/**
+ * An common Hierarchy Tree diagram component
+ *
+ * @param {boolean} hideAttribution an option to hide Attribution of React flow ref. link.
+ * The free tier already matches the current requirement.
+ * Unless the clients requires this, please only hide this attribution when you/clients are subscribed to React Flow Pro.
+ * @returns an "auto-layout" Hierarchy Tree diagram.
+ */
 export const HierarchyTree = ({ hideAttribution = false }: { hideAttribution?: boolean }) => {
   return (
     <ReactFlowProvider>

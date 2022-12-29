06cfceb46a526338c0d11ef0b319744ccf9d24c7
smlee97 <smlee@pentasecurity.com>
2022-12-29 13:55:48 +0900

    optimization code : AppHeader

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -14,16 +14,16 @@ import { ReactComponent as LogoDark } from '@/assets/icons/Logo.svg';
 import './AppHeader.scss';
 
 export const AppHeader = () => {
-    const { t } = useTranslation();
-  
-    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-    const isDarkMode = colorScheme === 'dark';
-  
-    const [toggleLang, setToggleLang] = useState(
-      localStorage.getItem('i18nextLng') || defaultLanguage,
-    );
+  const { t } = useTranslation();
 
-    /**
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const isDarkMode = colorScheme === 'dark';
+
+  const [toggleLang, setToggleLang] = useState(
+    localStorage.getItem('i18nextLng') || defaultLanguage,
+  );
+
+  /**
    * Currently support only 2 languages.
    * Use "Select" component instead of "ActionIcon" and
    * update languageConfig.lang value to map with remaining attributes if needed.
@@ -32,51 +32,51 @@ export const AppHeader = () => {
    *
    * @note Remove the "d-none" class of ActionIcon to show this feature.
    */
-    const renderToggleLang = languageConfig.map((langConfig) => (
-        <Tooltip key={langConfig.lang} withArrow label={langConfig.tooltip}>
-          <ActionIcon
-            className="header__lang d-none ml-1"
-            size="xl"
-            radius="xl"
-            onClick={() => setToggleLang(langConfig.lang)}
-            hidden={langConfig.lang === toggleLang}
-          >
-            <img src={langConfig.img} alt={langConfig.alt} />
-          </ActionIcon>
-        </Tooltip>
-      ));
+  const renderToggleLang = languageConfig.map((langConfig) => (
+    <Tooltip key={langConfig.lang} withArrow label={langConfig.tooltip}>
+      <ActionIcon
+        className="header__lang d-none ml-1"
+        size="xl"
+        radius="xl"
+        onClick={() => setToggleLang(langConfig.lang)}
+        hidden={langConfig.lang === toggleLang}
+      >
+        <img src={langConfig.img} alt={langConfig.alt} />
+      </ActionIcon>
+    </Tooltip>
+  ));
 
-      useEffect(() => {
-        i18n.changeLanguage(toggleLang);
-      }, [toggleLang]);
+  useEffect(() => {
+    i18n.changeLanguage(toggleLang);
+  }, [toggleLang]);
 
-      useEffect(() => {
-        if (i18n.resolvedLanguage === defaultLanguage && toggleLang !== defaultLanguage) {
-          setToggleLang(defaultLanguage);
-        }
-      }, []);
-    
-      return (
-        <Header height={60} className="px-3">
-          <Group sx={{ height: '100%' }} px={20} position="apart">
-            <Link className="header__logo" to={Path.HOMEPAGE}>
-              {isDarkMode ? <LogoDark /> : <Logo />}
-            </Link>
-            <div className="d-flex justify-between align-center">
-              <Tooltip withArrow label={t('common.theme.' + colorScheme)}>
-                <ActionIcon
-                  variant="outline"
-                  size="lg"
-                  radius="xl"
-                  color={isDarkMode ? 'yellow' : ''}
-                  onClick={() => toggleColorScheme()}
-                >
-                  {isDarkMode ? <MoonStars size={18} /> : <Sun size={18} />}
-                </ActionIcon>
-              </Tooltip>
-              {renderToggleLang}
-            </div>
-          </Group>
-        </Header>
-      );
-}
+  useEffect(() => {
+    if (i18n.resolvedLanguage === defaultLanguage && toggleLang !== defaultLanguage) {
+      setToggleLang(defaultLanguage);
+    }
+  }, []);
+
+  return (
+    <Header height={60} className="px-3">
+      <Group sx={{ height: '100%' }} px={20} position="apart">
+        <Link className="header__logo" to={Path.HOMEPAGE}>
+          {isDarkMode ? <LogoDark /> : <Logo />}
+        </Link>
+        <div className="d-flex justify-between align-center">
+          <Tooltip withArrow label={t('common.theme.' + colorScheme)}>
+            <ActionIcon
+              variant="outline"
+              size="lg"
+              radius="xl"
+              color={isDarkMode ? 'yellow' : ''}
+              onClick={() => toggleColorScheme()}
+            >
+              {isDarkMode ? <MoonStars size={18} /> : <Sun size={18} />}
+            </ActionIcon>
+          </Tooltip>
+          {renderToggleLang}
+        </div>
+      </Group>
+    </Header>
+  );
+};

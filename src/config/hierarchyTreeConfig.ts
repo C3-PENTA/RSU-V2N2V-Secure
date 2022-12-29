8f7886716d354ae211da45376fc387d80d0aab8a
smlee97 <smlee@pentasecurity.com>
2022-12-29 14:11:39 +0900

    update file : hierarchyTreeConfig

--- src/config/hierarchyTreeConfig.ts
+++ src/config/hierarchyTreeConfig.ts
@@ -4,3 +4,13 @@ export const rootCoordinate = { x: 0, y: 0 };
 
 export const defaultNodeWidth = 90;
 export const defaultNodeHeight = 50;
+
+export const NodeStatusConfig: IStatusConfig[] = [
+  { key: 'STOP', status: ENodeStatus.STOP, label: 'dashboard.status.stop', color: 'red' },
+  {
+    key: 'DRIVING',
+    status: ENodeStatus.DRIVING,
+    label: 'dashboard.status.driving',
+    color: 'green',
+  },
+];
